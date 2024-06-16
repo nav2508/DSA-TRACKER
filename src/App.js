4c66fd6b84e20352e7017ddb6ab426a792881084
Navya Manjari <navyamanjari.hyd@gmail.com>
2024-06-16 15:43:25 +0530

    Adding files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// App.js
+import DSAList from "./components/DSAList";
+
+export default function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center mt-4">
+        <h1 className="text-emerald-500 font-bold text-3xl">
+          DATA STRUCTURES AND ALGORITHMS
+        </h1>
+        <h3
+          className="bg-clip-text text-transparent bg-gradient-to-r 
+							from-purple-500 to-pink-500 font-bold text-xl mb-4"
+        >
+          DSA Tracker
+        </h3>
+        <DSAList />
+      </div>
+    </>
+  );
+}
